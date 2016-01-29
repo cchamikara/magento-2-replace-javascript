11bbfe29c5e10c3d21ef2538e7c1d67d95082e51
chamalC <chamalchamikara@gmail.com>
2016-01-29 08:45:54 +0530

    First commit

---
+++ app/code/Muaw/CustomSwatches/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Muaw_CustomSwatches',
+    __DIR__
+);
---
+++ app/code/Muaw/CustomSwatches/view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Chamal on 1/26/16.
+ */
+var config = {
+    map: {
+        '*': {
+            'Magento_Swatches/js/SwatchRenderer':'Muaw_CustomSwatches/js/SwatchRenderer'
+        }
+    }
+};
